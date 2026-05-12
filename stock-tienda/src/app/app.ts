b355631fb393daf2077069f8c04e531ebdb43fab
Jonnathan Noreña <jonatannorenagrisales@gmail.com>
2026-05-11 20:41:05 -0500

    Prototipo login JN

--- stock-tienda/src/app/app.ts
+++ stock-tienda/src/app/app.ts
@@ -1,12 +1,14 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { ReactiveFormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, ReactiveFormsModule],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
 export class App {
   protected title = 'stock-tienda';
 }
+
